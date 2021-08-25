6cd9cb81c5567ddd4e4016c2cb992bf16e43e1f4
mleblanc <mleblanc@student.42quebec.com>
2021-08-25 16:13:23 -0400

    norme

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mleblanc <mleblanc@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/17 16:03:37 by mleblanc          #+#    #+#             */
-/*   Updated: 2021/08/25 15:38:19 by mleblanc         ###   ########.fr       */
+/*   Updated: 2021/08/25 16:13:03 by mleblanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,6 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <stdbool.h>
-
-void	free_token(void *t);
 
 char	*get_line(char *line)
 {
@@ -36,6 +34,23 @@ char	*get_line(char *line)
 	return (line);
 }
 
+void	print_token_list(t_list *lst)
+{
+	t_token	*token;
+	t_list	*ptr;
+
+	if (lst)
+	{
+		ptr = lst;
+		while (ptr)
+		{
+			token = (t_token *)ptr->content;
+			printf("%s - %d\n", ft_str_data(token->value), token->type);
+			ptr = ptr->next;
+		}
+	}
+}
+
 void	init_tokenizer(t_tokenizer *tok)
 {
 	tok->cursor = 0;
@@ -46,9 +61,7 @@ void	init_tokenizer(t_tokenizer *tok)
 int	main(int argc, char **argv, char **env)
 {
 	t_list		*lst;
-	t_list		*ptr;
 	t_tokenizer	tok;
-	t_token		*token;
 
 	(void)argc;
 	(void)argv;
@@ -61,18 +74,9 @@ int	main(int argc, char **argv, char **env)
 		if (!*tok.str)
 			continue ;
 		lst = tokenize(&tok);
-		if (lst)
-		{
-			ptr = lst;
-			while (ptr)
-			{
-				token = (t_token *)ptr->content;
-				printf("%s - %d\n", ft_str_data(token->value), token->type);
-				ptr = ptr->next;
-			}
-		}
 		if (ft_strncmp("exit", tok.str, 5) == 0)
 			break ;
+		print_token_list(lst);
 		ft_lstclear(&lst, free_token);
 	}
 	free(tok.str);
