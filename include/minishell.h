0385212d402ff182375475898ede66f330944eec
mikastiv <m.leblanc_3@hotmail.com>
2021-08-24 21:40:54 -0400

    phrase

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mleblanc <mleblanc@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/18 15:16:25 by mleblanc          #+#    #+#             */
-/*   Updated: 2021/08/24 17:47:08 by mleblanc         ###   ########.fr       */
+/*   Updated: 2021/08/24 21:40:35 by mleblanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,12 @@
 
 # include <stdbool.h>
 
-# define SHELL_PROMPT ("minishell: ")
+# define SHELL_PROMPT "minishell: "
 
-typedef struct s_parse
+typedef struct s_phrase
 {
 	char		*name;
-	bool		is_builtin;
 	char		**args;
-	char		**opts;
-}	t_parse;
+}	t_phrase;
 
 #endif
