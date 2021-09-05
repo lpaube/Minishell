539decadb024925cb31b354517c035d2230591b1
LPaube <lpaube@hotmail.com>
2021-09-05 16:08:29 -0400

    operators functional except herestrings

--- src/exec_operation.c
+++ src/exec_operation.c
@@ -6,7 +6,7 @@
 /*   By: laube <louis-philippe.aube@hotmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/02 18:54:03 by laube             #+#    #+#             */
-/*   Updated: 2021/09/05 14:13:39 by laube            ###   ########.fr       */
+/*   Updated: 2021/09/05 15:45:37 by laube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,8 +50,12 @@ void	get_source(t_phrase *phrase)
 
 	if (phrase->prev && phrase->prev->op == PIPE)
 		src_pipe_read(phrase);
-	if (phrase->next && (phrase->next->op == INPUT || phrase->next->op == READ))
+	if (phrase->op == INPUT || phrase->op == READ)
+	{
+		if (!phrase->name)
+			phrase->name = "more";
 		src_red_input(phrase);
+	}
 }
 
 void	dest_pipe_write(t_phrase *phrase)
@@ -68,7 +72,10 @@ void	dest_red_output(t_phrase *phrase)
 {
 	int	open_fd;
 
-	open_fd = open(phrase->next->name, O_RDWR | O_CREAT);
+	if (phrase->op == OUTPUT)
+		open_fd = open(phrase->next->name, O_RDWR | O_CREAT);
+	if (phrase->op == APPEND)
+		open_fd = open(phrase->next->name, O_RDWR | O_APPEND | O_CREAT);
 	phrase->saved_stdout = dup(1);
 	dup2(open_fd, 1);
 	close(open_fd);
@@ -95,46 +102,6 @@ void	clean_fd(t_phrase *phrase)
 		close(phrase->saved_stdout);
 	}
 }
-// void	pipe_read(t_phrase *phrase)
-// {
-// 	int	saved_stdin;
-
-// 	saved_stdin = dup(0);
-// 	dup2(phrase->prev->fd[0], 0);
-// 	if (!phrase->next)
-// 		execution_control(phrase);
-// 	if (phrase->op == PIPE)
-// 		pipe_write(phrase);
-// 	dup2(saved_stdin, 0);
-// 	close(phrase->prev->fd[0]);
-// 	close(saved_stdin);
-// }
-
-// void	redirect_write(t_phrase *phrase)
-// {
-// 	int	open_fd;
-// 	int	saved_stdout;
-
-// 	if (phrase->op == APPEND)
-// 	{
-// 		open_fd = open(phrase->next->name, O_RDWR | O_APPEND | O_CREAT);
-// 	}
-// 	else if (phrase->op == OUTPUT)
-// 	{
-// 		open_fd = open(phrase->next->name, O_RDWR | O_CREAT);
-// 	}
-// 	else
-// 		return ;
-// 	//
-// 	//NEED TO CHANGE ACCESS TO FILE IF NEWLY CREATED
-// 	//
-// 	saved_stdout = dup(1);
-// 	dup2(open_fd, 1);
-// 	execution_control(phrase);
-// 	dup2(saved_stdout, 1);
-// 	close(saved_stdout);
-// 	close(open_fd);
-// }
 
 void	operation_control(t_phrase *phrase)
 {
