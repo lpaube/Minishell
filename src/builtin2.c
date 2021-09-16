4ae5a441015462155c5f0ececc36b39160204f59
mikastiv <m.leblanc_3@hotmail.com>
2021-09-16 09:25:45 -0400

    bug fix

--- src/builtin2.c
+++ src/builtin2.c
@@ -6,7 +6,7 @@
 /*   By: mleblanc <mleblanc@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/15 22:25:41 by mleblanc          #+#    #+#             */
-/*   Updated: 2021/09/15 23:10:44 by mleblanc         ###   ########.fr       */
+/*   Updated: 2021/09/16 09:14:51 by mleblanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,16 +49,20 @@ void	ft_export(t_node *node)
 {
 	t_string	var;
 	size_t		i;
+	char		*x;
 
 	if (ft_strarr_size(node->args) < 2)
 		return (ft_env());
 	if (!is_valid_var_name(node->args[1]))
 		return ;
-	var = ft_str_new_copy(node->args[1]);
+	var = ft_str_new(var_name(node->args[1]));
 	ft_str_add_back(var, '=');
+	char *tmp = ft_str_data(var);
+	(void)tmp;
 	i = 0;
 	while (g_minishell.env[i])
 	{
+		x = g_minishell.env[i];
 		if (ft_str_cmp_cstr(var, g_minishell.env[i], ft_str_len(var)) == 0)
 		{
 			free(g_minishell.env[i]);
