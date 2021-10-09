730b12871d02716042f43f164f38b64bb17b171a
mikastiv <m.leblanc_3@hotmail.com>
2021-10-09 00:55:22 -0400

    libft

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mleblanc <mleblanc@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/08 13:33:08 by mleblanc          #+#    #+#             */
-/*   Updated: 2021/10/06 11:14:35 by mleblanc         ###   ########.fr       */
+/*   Updated: 2021/10/09 00:54:15 by mleblanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,10 +40,10 @@
 
 typedef struct s_minishell
 {
-	char	**env;
-	t_u32	code;
-	int		stdin_fd;
-	int		stdout_fd;
+	char		**env;
+	uint32_t	code;
+	int			stdin_fd;
+	int			stdout_fd;
 }	t_minishell;
 
 extern t_minishell	g_mini;
